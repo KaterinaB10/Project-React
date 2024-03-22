eb36fd267c88c31f2f887a4179b6f2c2578e0d40
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-22 09:39:02 +0100

    Created Layout.jsx as a parent route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./pages/Layout";
 
 function App() {
   return (
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
