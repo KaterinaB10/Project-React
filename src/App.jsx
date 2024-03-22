2b279afe8dac4a3f531f11d8ccc147e71dbf533f
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-22 10:07:05 +0100

    Created main routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { LandingPage } from "./pages/LandingPage";
+import { AboutUsPage } from "./pages/about-us-page/AboutUsPage";
+import { LoginPage } from "./pages/login-page/LoginPage";
+import { SignUpPage } from "./pages/sign-up-page/SignUpPage";
+import { ArticlesPage } from "./pages/articles/ArticlesPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -9,8 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route index element={<LandingPage />} />
-        <Route />
-        <Route />
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/article/:id" element={<ArticlesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
