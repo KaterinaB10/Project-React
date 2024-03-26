11ad4a7e2ac41cf0439f118c70145568f1db3225
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-26 14:41:07 +0100

    Mobile and Web version of nav is done

---
+++ src/components/nav-bar/Logo.jsx
@@ -0,0 +1,9 @@
+import logo from "../../assets/images/Logo.svg";
+
+export function Logo() {
+  return (
+    <div className="logo">
+      <img src={logo} alt="" height={70.66} width={192} />
+    </div>
+  );
+}
--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,9 +1,11 @@
 import { NavButtons } from "./NavButtons";
+import { Logo } from "./Logo";
 import "./navBar.css";
 
 export function NavBar() {
   return (
     <div className="fixed-navbar">
+      <Logo />
       <NavButtons />
     </div>
   );
