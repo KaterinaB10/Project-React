1d48bbe667e28e8d32f8d77fb2e8548cebfff266
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-26 13:01:11 +0100

    .

---
+++ src/components/nav-bar/NavBar.jsx
@@ -0,0 +1,9 @@
+import { NavButtons } from "./NavButtons";
+
+export function NavBar() {
+  return (
+    <nav className="fixed-navbar">
+      <NavButtons />
+    </nav>
+  );
+}
--- src/components/nav-bar/NavButtons.jsx
+++ src/components/nav-bar/NavButtons.jsx
@@ -1,3 +1,40 @@
+import { NavLink } from "react-router-dom";
+
 export function NavButtons() {
-  return <NavButtons></NavButtons>;
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about-us"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        About Us
+      </NavLink>
+      <NavLink
+        to=""
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        Discord Community
+      </NavLink>
+      <NavLink
+        to="/login"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        Login
+      </NavLink>
+    </nav>
+  );
 }
