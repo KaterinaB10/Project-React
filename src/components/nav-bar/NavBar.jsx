ce4a4eba7197b9739ab16fedb84c67bee1bb4524
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-29 14:08:13 +0100

    themeswitcher is done

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,12 +1,16 @@
 import { NavButtons } from "./NavButtons";
 import { Logo } from "./Logo";
 import "./navBar.css";
+import { ThemeSwitcher } from "../theme-switcher/ThemeSwitcher";
 
 export function NavBar() {
   return (
     <div className="fixed-navbar">
       <Logo />
-      <NavButtons />
+      <div className="buttons-container">
+        <NavButtons />
+        <ThemeSwitcher />
+      </div>
     </div>
   );
 }
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,24 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../components/nav-bar/NavBar";
-import { UseTheme } from "../hooks/UseTheme";
 
 export function Layout() {
-  const { setTheme } = UseTheme("light");
-
-  const handleLightThemeClick = () => {
-    setTheme("light");
-  };
-  const handleDarkThemeClick = () => {
-    setTheme("dark");
-  };
-
   return (
     <>
       <header className="header">
         <NavBar />
       </header>
-      <button onClick={handleLightThemeClick}>1</button>
-      <button onClick={handleDarkThemeClick}>2</button>
       <main>
         <Outlet />
       </main>
