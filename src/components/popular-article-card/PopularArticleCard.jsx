5414e4d55ec0f9b4d1c5f74f550b663fd0efaf83
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-04 12:47:21 +0200

    article was styled

--- src/components/popular-article-card/PopularArticleCard.jsx
+++ src/components/popular-article-card/PopularArticleCard.jsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router-dom";
+import arrowReadMore from "../../assets/images/arrow-read-more.svg";
+import articleImage from "../../assets/images/Image-article.svg";
 import "./popularArticleCard.css";
 
 export function PopularArticleCard() {
   return (
-    <div className="popular-article-card">
+    <section className="popular-article-card">
       <div className="popular-article-image-container">
-        <img src="" alt="" />
+        <img src={articleImage} alt="" />
       </div>
-      <div className="popular-article-title-container">
-        <h1>Introducing to coding</h1>
-        <p>Short description of Introducing to coding post.</p>
+      <div className="popular-article-info-container">
+        <div className="popular-article-title-container">
+          <h1>Introducing to coding</h1>
+          <p>Short description of Introducing to coding post.</p>
+        </div>
+        <div className="popular-article-additional-container">
+          <p>03.04.2024</p>
+          <Link to="/about-us" className="read-more-link">
+            <img src={arrowReadMore} alt="" />
+          </Link>
+        </div>
       </div>
-      <div className="popular-article-additional-container">
-        <p>03.04.2024</p>
-        <Link>R</Link>
-      </div>
-    </div>
+    </section>
   );
 }
