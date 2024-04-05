07c97a292082cd8c96132b8b125d69eb17355163
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-05 12:38:09 +0200

    styling of search field is done

--- src/components/search-input/SearchInput.jsx
+++ src/components/search-input/SearchInput.jsx
@@ -1,10 +1,17 @@
 import "@radix-ui/themes/styles.css";
 import { TextField } from "@radix-ui/themes";
+import searchIcon from "../../assets/images/icon-search.svg";
+import "./searchInput.css";
 
 export function SearchInput() {
   return (
-    <TextField.Root placeholder="Search the docs…">
-      <TextField.Slot></TextField.Slot>
+    <TextField.Root
+      placeholder="Write down which information you are looking for"
+      className="search-field"
+    >
+      <TextField.Slot side="right" className="search-icon-container">
+        <img src={searchIcon} alt="" />
+      </TextField.Slot>
     </TextField.Root>
   );
 }
