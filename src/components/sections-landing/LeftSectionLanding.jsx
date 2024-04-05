0ed92fdabd935ea3a2b02f2d6da11eea71c337a3
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-05 15:41:42 +0200

    done

--- src/components/sections-landing/LeftSectionLanding.jsx
+++ src/components/sections-landing/LeftSectionLanding.jsx
@@ -1,12 +1,14 @@
 import { TitleOfSection } from "../titles-for-sections/TitleOfSection";
 import { ArticlesList } from "../arlicles/ArticlesList";
 import { SearchInput } from "../search-input/SearchInput";
+import { MainArticle } from "../arlicles/MainArticle";
 import "./leftSideSection.css";
 
 export function LeftSectionLanding() {
   return (
     <section className="left-side-section">
       <SearchInput />
+      <MainArticle />
       <TitleOfSection title="All posts" />
       <ArticlesList />
     </section>
