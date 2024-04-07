3bfc26d601786e361ccd3c6a9e7842badb37d830
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-07 17:43:08 +0200

    tagbuttons created

--- src/components/sections-landing/LeftSectionLanding.jsx
+++ src/components/sections-landing/LeftSectionLanding.jsx
@@ -2,12 +2,14 @@ import { TitleOfSection } from "../titles-for-sections/TitleOfSection";
 import { ArticlesList } from "../arlicles/ArticlesList";
 import { SearchInput } from "../search-input/SearchInput";
 import { MainArticle } from "../arlicles/MainArticle";
+import { TagButtons } from "../tag-buttons/TagButtons";
 import "./leftSideSection.css";
 
 export function LeftSectionLanding() {
   return (
     <section className="left-side-section">
       <SearchInput />
+      <TagButtons />
       <MainArticle />
       <TitleOfSection title="All posts" />
       <ArticlesList />
---
+++ src/components/tag-buttons/TagButtons.jsx
@@ -0,0 +1,26 @@
+import articles from "../../articlesData.json";
+import "./tagButtons.css";
+
+export function TagButtons() {
+  const uniqueTags = new Set();
+
+  articles.articles.forEach((article) => {
+    article.tagList.forEach((tag) => {
+      uniqueTags.add(tag);
+    });
+  });
+
+  const uniqueTagList = Array.from(uniqueTags);
+
+  return (
+    <div className="tag-buttons-container">
+      <ul className="buttons-list">
+        {uniqueTagList.map((tag, index) => (
+          <li key={index}>
+            <button>{tag}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
