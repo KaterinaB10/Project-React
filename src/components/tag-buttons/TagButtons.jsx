5897f502eac32418ddca8dd955d74acc38e64cfb
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-08 13:49:21 +0200

    updated

--- src/components/tag-buttons/TagButtons.jsx
+++ src/components/tag-buttons/TagButtons.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import articles from "../../articlesData.json";
 import "./tagButtons.css";
 
 export function TagButtons() {
+  const [activeClass, setActiveClass] = useState(null);
+
   const uniqueTags = new Set();
 
   articles.articles.forEach((article) => {
@@ -12,12 +15,27 @@ export function TagButtons() {
 
   const uniqueTagList = Array.from(uniqueTags);
 
+  const [hover, setHover] = useState(false);
+
   return (
     <div className="tag-buttons-container">
       <ul className="buttons-list">
         {uniqueTagList.map((tag, index) => (
           <li key={index}>
-            <button className="tag-btn">#{tag}</button>
+            <button
+              className={
+                activeClass === tag
+                  ? "active-tag"
+                  : hover
+                  ? "hovered-tag"
+                  : "tag-btn"
+              }
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+              onClick={() => setActiveClass(tag)}
+            >
+              #{tag}
+            </button>
           </li>
         ))}
       </ul>
